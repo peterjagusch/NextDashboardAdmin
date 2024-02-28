eff048addf945b7a1e4884637db81c825f9588cf
Peter Jagusch <peter_jagusch@hotmail.com>
2024-02-28 13:59:43 +0000

    First commit
    First commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'media.licdn.com',
+            port: '',
+            pathname: '/dms/**',
+          },
+        ],
+      },
+    
+}
+
+module.exports = nextConfig
